2820c6c2e0de60aed1ba5583758466ca22e57ece
Ubuntu <ubuntu@ip-172-31-31-164.ec2.internal>
2022-07-19 17:00:52 +0000

    Finished

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,12 @@ impl Board {
         }
     }
 
-    fn make_move(&mut self, turn_number: i8, letter: String) {
-        let (r, c) = Self::get_index_from_letter(&self, letter);
+    fn make_move(&mut self, turn_number: i8, letter: &String) {
+        let (r, c) = Self::get_index_from_letter(&self, &letter);
 
         if r > self.board.len() {
             println!("Invalid move... dumbass");
+            return
         }
 
         if self.board[r][c] != -1i8 {
@@ -30,17 +31,19 @@ impl Board {
         }
     }
 
-    fn get_index_from_letter(&self, letter: String) -> (usize, usize) {
-        let move_letters: [[String; 3]; 3] = [["a", "b", "c"], ["d", "e", "f"], ["g", "h", "i"]];
-
-        for i in 0..move_letters.len() {
-            for j in 0..move_letters[i].len() {
-                if move_letters[i][j] == letter {
-                    return (i, j)
-                }
-            }
+    fn get_index_from_letter(&self, letter: &String) -> (usize, usize) {
+        match letter.trim() {
+            "a" => return (0, 0),
+            "b" => return (0, 1),
+            "c" => return (0, 2),
+            "d" => return (1, 0),
+            "e" => return (1, 1),
+            "f" => return (1, 2),
+            "g" => return (2, 0),
+            "h" => return (2, 1),
+            "i" => return (2, 2),
+            _ => return (self.board.len() + 1, 0)
         }
-        return (self.board.len() + 1, 0)
     }
 
     fn check_for_wins(&self) -> bool {
@@ -63,11 +66,13 @@ impl Board {
             }
         }
         // Check for diagonals
-        if self.board[0][0] == self.board[1][1] && self.board[1][1] == self.board[2][2] {
-            return true
-        }
-        if self.board[0][2] == self.board[1][1] && self.board[1][1] == self.board[2][0] {
-            return true
+        if self.board[1][1] != -1i8 {
+            if self.board[0][0] == self.board[1][1] && self.board[1][1] == self.board[2][2] {
+                return true
+            }
+            if self.board[0][2] == self.board[1][1] && self.board[1][1] == self.board[2][0] {
+                return true
+            }
         }
 
         return false
@@ -104,8 +109,8 @@ impl Board {
                         _ => print!("{} | ", move_letters[i][j]),
                     }
                     _ => match j {
-                        2 => println!("{}", self.board[i][j]),
-                        _ => print!("{} | ", self.board[i][j]),
+                        2 => println!("{}", " "),
+                        _ => print!("{} | ", " "),
                     }
                 }
                 if j == 2 && i != 2 {
@@ -128,19 +133,55 @@ fn get_user_input() -> String {
 
 fn main() {
     let mut game_board = Board::new();
-    let mut playing = true;
+    let mut has_won = false;
     let mut turn = 0;
-    let mut player_move = String::new();
+    let mut player_move;
 
-    while playing {
+    while !has_won {
+        // Check board
+        has_won = game_board.check_for_wins();
+        if has_won == true {
+            game_board.print();
+            if turn % 2 == 0 {
+                println!("Player 0 wins!")
+            } else {
+                println!("Player 1 wins!")
+            }
+            continue
+        }
+        if turn == 9 {
+            println!("Losers");
+            has_won = true;
+            continue
+        }
+
+        // Clear terminal
+        print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+
+        // Print board
         println!("Current board:");
         game_board.print();
+        println!();
         println!("Possible moves:");
         game_board.print_moves();
-        print!("Enter which move you'd like to make: ");
+        println!();
+
+        // State which user is making the move
+        if turn % 2 == 0 {
+            println!("Player 0's turn")
+        } else {
+            println!("Player 1's turn")
+        }
+
+        // Get user's move
+        println!();
         player_move = get_user_input();
-        game_board.make_move(turn, player_move);
-        playing = !game_board.check_for_wins();
+
+        // Make move
+        game_board.make_move(turn, &player_move);
+        println!();
+
+        // Inc turn
         turn += 1;
     }
 }
